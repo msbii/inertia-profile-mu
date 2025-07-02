a9532c91842449116b21688f1279591baaece628
Msbii <abidinsyafri51@gmail.com>
2025-07-02 12:49:28 +0700

    -

--- app/Http/Controllers/DashboardInventarisController.php
+++ app/Http/Controllers/DashboardInventarisController.php
@@ -108,8 +108,8 @@ class DashboardInventarisController extends Controller
     public function edit(Inventaris $inventaris)
     {
         //
+        dd($inventaris);
         $data = inventaris::where('slug', $inventaris)->firstOrFail();
-        dd($data);
         return Inertia::render('dashboard/inventaris/edit',[
             'post' => $inventaris,
             'categories' => Lingkup::take(2)->get(),
