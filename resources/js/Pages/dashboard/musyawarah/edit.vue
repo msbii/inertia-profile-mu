08326f88a838301541c558cf0518c07a7e1fcb6e
Msbii <abidinsyafri51@gmail.com>
2025-07-02 10:41:56 +0700

    -

--- resources/js/Pages/dashboard/musyawarah/edit.vue
+++ resources/js/Pages/dashboard/musyawarah/edit.vue
@@ -178,8 +178,8 @@ const form = useForm({
     oldImage: props.post?.image || "",
     time: props.post?.time || "",
     location: props.post?.location || "",
-    agenda: props.post?.agenda || "",
-    hasil: props.post?.hasil || "",
+    agenda: props.post?.hasil || "",
+    hasil: props.post?.agenda || "",
 });
 
 function generateSlug() {
