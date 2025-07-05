9cc969517fad420e3b378f139c219933ecd4c0f7
Msbii <abidinsyafri51@gmail.com>
2025-07-05 15:33:03 +0700

    -

--- resources/js/Components/headerComponent.vue
+++ resources/js/Components/headerComponent.vue
@@ -276,7 +276,7 @@
                             >
                                 <Link
                                     href="/dashboard/home"
-                                    class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+                                    class="text-lg py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
                                 >
                                     My Dashboard
                                 </Link>
@@ -287,7 +287,7 @@
                                     href="/logout"
                                     method="post"
                                     as="button"
-                                    class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+                                    class="text-lg py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
                                 >
                                     Logout
                                 </Link>
