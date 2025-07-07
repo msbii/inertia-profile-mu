1f0077f5884eecce1dcfcd473de5d44affd4ad92
Msbii <abidinsyafri51@gmail.com>
2025-07-07 22:29:11 +0700

    -

--- resources/js/Components/Headers/HeaderStats.vue
+++ resources/js/Components/Headers/HeaderStats.vue
@@ -1,13 +1,6 @@
 <template>
     <!-- Header -->
+    <div class="relative bg-emerald-600 md:pt-32 pb-32 pt-12"></div>
 </template>
 
-<script>
-import CardStats from "@/components/Cards/CardStats.vue";
-
-export default {
-    components: {
-        CardStats,
-    },
-};
-</script>
+<script></script>
