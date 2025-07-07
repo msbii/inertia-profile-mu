1de3348a6aeb90f66e93719bc07121d05c6a2386
Msbii <abidinsyafri51@gmail.com>
2025-07-07 22:27:22 +0700

    -

--- resources/js/shared/Admin.vue
+++ resources/js/shared/Admin.vue
@@ -3,8 +3,8 @@
         <Sidebar />
         <div class="relative md:ml-64 bg-blueGray-100">
             <admin-navbar />
-            <header-stats />
-            <div class="pt-20 px-4 md:px-10 mx-auto w-full -m-24">
+            <!-- <header-stats /> -->
+            <div class="px-4 md:px-10 mx-auto w-full -m-24">
                 <slot />
                 <footer-admin />
             </div>
@@ -14,7 +14,7 @@
 <script>
 import AdminNavbar from "@/Components/Navbars/AdminNavbar.vue";
 import Sidebar from "@/Components/Sidebar/Sidebar.vue";
-import HeaderStats from "@/Components/Headers/HeaderStats.vue";
+// import HeaderStats from "@/Components/Headers/HeaderStats.vue";
 import FooterAdmin from "@/Components/Footers/FooterAdmin.vue";
 
 export default {
@@ -22,7 +22,7 @@ export default {
     components: {
         Sidebar,
         AdminNavbar,
-        HeaderStats,
+        // HeaderStats,
         FooterAdmin,
     },
 };
