d7955820aca3ff13d8d8c6d3a5c115daf78f5abc
Msbii <abidinsyafri51@gmail.com>
2025-07-08 20:37:43 +0700

    -

--- resources/js/Components/Dropdowns/UserDropdown.vue
+++ resources/js/Components/Dropdowns/UserDropdown.vue
@@ -1,26 +1,34 @@
 <template>
-    <div>
-        <!-- Trigger -->
+    <div class="relative z-50">
         <a
-            class="text-white text-sm uppercase font-semibold cursor-pointer"
+            class="text-blueGray-500 block"
             href="#"
             :ref="btnDropdownRef"
             @click="toggleDropdown"
         >
-            <i class="fas fa-user"></i> Halo, <strong>{{ user.name }}</strong>
+            <div class="link-box">
+                <span
+                    class="text-white text-sm uppercase hidden lg:inline-block font-semibold"
+                >
+                    <i class="fas fa-user"></i>
+                    Halo, <strong>{{ user.name }}</strong>
+                </span>
+            </div>
         </a>
 
-        <!-- Dropdown -->
         <div
             :ref="popoverDropdownRef"
-            class="absolute right-0 mt-2 bg-white text-base z-50 py-2 list-none text-left rounded shadow-lg min-w-48"
-            :class="dropdownPopoverShow ? 'block' : 'hidden'"
+            class="absolute right-0 mt-2 bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
+            :class="{
+                hidden: !dropdownPopoverShow,
+                block: dropdownPopoverShow,
+            }"
         >
             <Link
                 href="/logout"
                 method="post"
                 as="button"
-                class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700 hover:bg-gray-100"
+                class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
             >
                 Logout
             </Link>
@@ -29,19 +37,24 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
 import { Link, usePage } from "@inertiajs/vue3";
 import { createPopper } from "@popperjs/core";
 
+// Ambil data user dari inertia
 const { props } = usePage();
 const user = props.auth.user;
 
+// State untuk dropdown
 const dropdownPopoverShow = ref(false);
+
+// Refs untuk popper
 const btnDropdownRef = ref(null);
 const popoverDropdownRef = ref(null);
 
-const toggleDropdown = (e) => {
-    e.preventDefault();
+// Toggle dropdown
+const toggleDropdown = (event) => {
+    event.preventDefault();
     dropdownPopoverShow.value = !dropdownPopoverShow.value;
 
     if (dropdownPopoverShow.value) {
--- resources/js/Components/Navbars/AdminNavbar.vue
+++ resources/js/Components/Navbars/AdminNavbar.vue
@@ -4,17 +4,42 @@
         class="fixed top-0 left-0 w-full z-30 bg-emerald-600 md:ml-64 flex items-center p-4"
     >
         <div
-            class="w-full mx-auto flex justify-between items-center md:px-10 px-4"
+            class="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4"
         >
             <!-- Brand -->
             <a
                 class="text-white text-sm uppercase hidden lg:inline-block font-semibold"
                 href="/"
             >
-                <i class="fas fa-home"></i> Beranda
+                <i class="fas fa-home"></i>
+                Beranda
             </a>
+            <!-- <a
+                class="text-white text-sm uppercase hidden lg:inline-block font-semibold"
+                href="javascript:void(0)"
+            >
+                Dashboard Selamat datang, <strong>{{ user?.name }}</strong
+                >!
+            </a> -->
 
-            <!-- User Dropdown -->
+            <!-- Form -->
+            <!-- <form
+                class="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3"
+            >
+                <div class="relative flex w-full flex-wrap items-stretch">
+                    <span
+                        class="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3"
+                    >
+                        <i class="fas fa-search"></i>
+                    </span>
+                    <input
+                        type="text"
+                        placeholder="Search here..."
+                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring w-full pl-10"
+                    />
+                </div>
+            </form> -->
+            <!-- User -->
             <ul
                 class="relative flex-col md:flex-row list-none items-center hidden md:flex"
             >
@@ -22,8 +47,14 @@
             </ul>
         </div>
     </nav>
+    <!-- End Navbar -->
 </template>
 
 <script setup>
 import UserDropdown from "@/Components/Dropdowns/UserDropdown.vue";
+
+import { usePage } from "@inertiajs/vue3";
+
+const page = usePage();
+const user = page.props.auth.user;
 </script>
