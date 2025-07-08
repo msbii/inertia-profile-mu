1c7e381fc99f1db3875b88b80fb407a1a95f7333
Msbii <abidinsyafri51@gmail.com>
2025-07-08 22:37:23 +0700

    -

--- resources/js/shared/Admin.vue
+++ resources/js/shared/Admin.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <Sidebar />
-        <div class="relative md:ml-64 bg-blueGray-100 pt-16">
+        <div class="pt-16 md:ml-64 bg-blueGray-100 min-h-screen">
             <admin-navbar />
             <!-- <header-stats /> -->
-            <div class="px-4 md:px-10 mx-auto w-full -m-24">
+            <div class="px-4">
                 <slot />
                 <footer-admin />
             </div>
