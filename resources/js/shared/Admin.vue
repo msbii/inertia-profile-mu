2710c8b23e732f18b9a68e41ef17b5866d1a72d6
Msbii <abidinsyafri51@gmail.com>
2025-07-10 10:34:33 +0700

    -

--- resources/js/shared/Admin.vue
+++ resources/js/shared/Admin.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <Sidebar />
-        <div class="relative md:ml-64 bg-blueGray-100 pt-16">
+        <div class="relative w-full md:ml-64 bg-blueGray-100 pt-16">
             <admin-navbar />
             <!-- <header-stats /> -->
             <div class="px-4 md:px-10 mx-auto w-full -m-24">
