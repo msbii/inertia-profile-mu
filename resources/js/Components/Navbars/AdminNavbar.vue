fd046ee022ba3ac0645b3a675bf705c229de1497
Msbii <abidinsyafri51@gmail.com>
2025-07-14 15:46:38 +0700

    -

--- resources/js/Components/Navbars/AdminNavbar.vue
+++ resources/js/Components/Navbars/AdminNavbar.vue
@@ -21,7 +21,7 @@
             </button>
             <div
                 v-if="showDropdown"
-                class="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded z-[999]"
+                class="w-full absolute right-0 mt-2 w-48 bg-white shadow-lg rounded z-[999]"
             >
                 <Link
                     href="/logout"
