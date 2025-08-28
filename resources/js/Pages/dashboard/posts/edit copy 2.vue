ec6f6c8e72a9cc9e23dfd8a3d5a5e38701f88a50
Msbii <abidinsyafri51@gmail.com>
2025-08-28 21:38:53 +0700

    -

--- resources/js/Pages/dashboard/posts/edit copy 2.vue
+++ resources/js/Pages/dashboard/posts/edit copy 2.vue
@@ -196,10 +196,7 @@ onMounted(() => {
                     Perbarui Postingan
                 </button> -->
                 <!-- Submit -->
-                <button
-                    type="submit"
-                    class="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded"
-                >
+                <button type="submit" class="btn btn-primary">
                     perbarui Postingan
                 </button>
             </form>
