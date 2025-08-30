567e25bfc4b11ed43dd91ad5fc4ac681ab79d8c4
Msbii <abidinsyafri51@gmail.com>
2025-08-30 15:33:16 +0700

    -

--- resources/js/Pages/dashboard/posts/index.vue
+++ resources/js/Pages/dashboard/posts/index.vue
@@ -75,7 +75,7 @@
                                         #
                                     </th>
                                     <th
-                                        class="px-6 py-3 text-xs font-semibold uppercase border text-left max-w-[75px] truncate"
+                                        class="px-6 py-3 text-xs font-semibold uppercase border text-left max-w-[100px]"
                                     >
                                         Judul
                                     </th>
